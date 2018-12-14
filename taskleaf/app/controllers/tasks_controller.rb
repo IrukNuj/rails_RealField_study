9c1a480ab16bfab666e9e92f2985d48bf3a03c05
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-14 20:31:14 +0900

    indexのびゅーto

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
@@ -16,6 +17,7 @@ class TasksController < ApplicationController
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク#{task.name}を追加しました。"
+    # redirect_to tasks_url, flash: {notice: "タスク#{task.name}を追加しました。"}
   end
 
   private
