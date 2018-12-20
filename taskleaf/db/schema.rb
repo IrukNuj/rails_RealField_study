64132c63e17f9078a21fab4f78020cc3e408d89a
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-20 14:06:24 +0900

    migrationの生成

--- taskleaf/db/schema.rb
+++ taskleaf/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_105152) do
+ActiveRecord::Schema.define(version: 2018_12_20_045744) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
