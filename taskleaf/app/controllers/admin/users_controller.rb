9d70a84002a3ae5606abb478dcf48e4c47af9b24
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-12-23 14:56:32 +0900

    make user controllers

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -7,19 +7,38 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_users_path ,notice: "ユーザー「#{@user.name}」を保存しました"
+      redirect_to admin_user_path(@user) ,notice: "ユーザー「#{@user.name}」を保存しました"
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @user = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params) # @userの中身を user_paramsで置き換えて.updateを走らせる
+      redirect_to admin_user_path(@user), notice: "ユーザーの更新をしました"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User,find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー消したでー"
   end
 
   private
